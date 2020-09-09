387e37dca610d4de0e1b2d92c4984c26a9d731a4
Dawid <dawid.bakalarski@gmail.com>
2020-09-09 17:25:03 +0200

    Updates

---
+++ precache-manifest.58be77452b8bf53e206b5fe5b24f3106.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32c9936cf2b4297ae85c28b9f970599b",
+    "url": "/FavMusicList/index.html"
+  },
+  {
+    "revision": "8b5a7304ec89be246c90",
+    "url": "/FavMusicList/static/css/main.852e29e1.chunk.css"
+  },
+  {
+    "revision": "685344c15dfbb65d761d",
+    "url": "/FavMusicList/static/js/2.8d357519.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/FavMusicList/static/js/2.8d357519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b5a7304ec89be246c90",
+    "url": "/FavMusicList/static/js/main.f4696573.chunk.js"
+  },
+  {
+    "revision": "9abfe222d9dfb5d8aa40",
+    "url": "/FavMusicList/static/js/runtime-main.874e8bfc.js"
+  }
+]);
